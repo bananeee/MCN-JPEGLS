e5098f162821fd37a53b97d4c09aa41aeb3ff7e1
ChimSe325 <dolong144@gmail.com>
2020-12-05 17:03:44 +0700

    golomb encoder

--- Encoder/golomb_encoder.py
+++ Encoder/golomb_encoder.py
@@ -1,8 +1,19 @@
-def golomb_encoder(error_arr):
-    string = ''
-    for i in range (0,len(error_arr[0])):
-        for j in range (0, len(error_arr)):
-            s = error_arr[i][j]
-            m = 7
+def golomb_encoder(k,s):
+    str = ''
+    m = 2**k
+    r = s % m
+    q = s // m
+    str = ''
+    for i in range(0,q):
+        str +='1'
+    str += '0'
+    temp = "{0:b}".format(r)
+    for i in range(0,k-len(temp)):
+        str += '0'
+    str += temp
+    return str
+
+
+
             
 
